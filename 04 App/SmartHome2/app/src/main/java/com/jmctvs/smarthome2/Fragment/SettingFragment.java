66041178936e857b3537f95e9afde200c990c699
lvminh97 <lvminh97@gmail.com>
2021-09-08 17:46:17 +0700

    202109081745 Add Setting view
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- 04 App/SmartHome2/app/src/main/java/com/jmctvs/smarthome2/Fragment/SettingFragment.java
+++ 04 App/SmartHome2/app/src/main/java/com/jmctvs/smarthome2/Fragment/SettingFragment.java
@@ -6,8 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.jmctvs.smarthome2.Utilities.Utilities;
-import com.jmctvs.thibanglaixe.R;
+import com.jmctvs.smarthome2.R;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -19,15 +18,13 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
     private Button a1Btn, a2Btn, b1Btn;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        view = inflater.inflate(R.layout.fragment_thithu_pack_select, container, false);
+        view = inflater.inflate(R.layout.fragment_setting, container, false);
         initUI();
         return view;
     }
 
     private void initUI(){
-        a1Btn = view.findViewById(R.id.btn_thithu_A1);
-        a2Btn = view.findViewById(R.id.btn_thithu_A2);
-        b1Btn = view.findViewById(R.id.btn_thithu_B1);
+
 
         a1Btn.setOnClickListener(this);
         a2Btn.setOnClickListener(this);
@@ -37,25 +34,7 @@ public class SettingFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_thithu_A1:
-                Utilities.setPack("A1");
-                break;
-            case R.id.btn_thithu_A2:
-                Utilities.setPack("A2");
-                break;
-            case R.id.btn_thithu_B1:
-                Utilities.setPack("B1");
-                break;
-        }
-        loadThiThuQuesSelect();
-    }
 
-    private void loadThiThuQuesSelect(){
-        Utilities.setCurrentScreen("ThiThuQuesSelect");
-        Fragment fragment = new ThiThuQuesSelectFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.nav_host_framelayout, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        }
     }
 }
